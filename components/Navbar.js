3a6b6ba0c7013308de5ad0f54cdaeddd57290ea8
Pragye Nawani <72926674+PragyeNawani@users.noreply.github.com>
2024-09-04 11:50:54 +0530

    Update Navbar.js
    Navham Functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useEffect } from 'react'
 const Navbar = () => {
     const activatenavtab = (index) => { 
@@ -9,7 +10,7 @@ const Navbar = () => {
         if (oldtab) oldtab.classList.remove("activenav")
         x[index].classList.add("activenav")
      }
-    
+    const [navham, setNavham] = useState(false)
   return (
 <>
 
@@ -20,28 +21,28 @@ const Navbar = () => {
         <img src="pfp2.jpg" className="h-10 w-10 rounded-full object-cover object-top" alt="Flowbite Logo" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-yellow-100">Pragye Nawani</span>
     </Link>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button onClick={()=>{setNavham(!navham)}} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div className="hidden w-full lg:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  md:gap-4 nav">
+    <div className={`${navham?"block":"hidden"} w-full lg:block lg:w-auto pt-5 lg:pt-0`} id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0  lg:gap-4 nav">
         <li>
-          <Link onClick={()=>{activatenavtab(0)}} href="#aboutme" className="block py-2 px-3 text-white rounded md:bg-transparent md:p-0 navitem activenav" aria-current="page">About Me</Link>
+          <Link onClick={()=>{activatenavtab(0)}} href="#aboutme" className="block py-2 px-3 text-white rounded lg:bg-transparent lg:p-0 navitem activenav" aria-current="page">About Me</Link>
         </li>
         <li>
-          <Link onClick={()=>{activatenavtab(1)}} href="#skills" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-100 md:p-0 navitem">Skills</Link>
+          <Link onClick={()=>{activatenavtab(1)}} href="#skills" className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-100 lg:p-0 navitem">Skills</Link>
         </li>
         <li>
-          <Link onClick={()=>{activatenavtab(2)}} href="#websitetypes" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-100 md:p-0 navitem">Websites Type</Link>
+          <Link onClick={()=>{activatenavtab(2)}} href="#websitetypes" className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-100 lg:p-0 navitem">Websites Type</Link>
         </li>
         <li>
-          <Link onClick={()=>{activatenavtab(3)}} href="#projects" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-100 md:p-0 navitem">Projects</Link>
+          <Link onClick={()=>{activatenavtab(3)}} href="#projects" className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-100 lg:p-0 navitem">Projects</Link>
         </li>
         <li>
-          <Link onClick={()=>{activatenavtab(4)}} href="#contact" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-100 md:p-0 navitem">Contact Me</Link>
+          <Link onClick={()=>{activatenavtab(4)}} href="#contact" className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-100 lg:p-0 navitem">Contact Me</Link>
         </li>
       </ul>
     </div>
